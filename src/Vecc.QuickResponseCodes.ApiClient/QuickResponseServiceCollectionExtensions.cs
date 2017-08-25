6828f3c00f07eeb41ae9ad10a52c1f0798d5a168
Edward Cooke <edward@veccsolutions.com>
2017-08-25 09:58:10 -0600

    Added service collection extensions so it's easy to bind everything

---
+++ src/Vecc.QuickResponseCodes.ApiClient/QuickResponseServiceCollectionExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Vecc.QuickResponseCodes.Abstractions;
+using Vecc.QuickResponseCodes.ApiClient;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class QuickResponseCodesServiceCollectionExtensions
+    {
+        public static IServiceCollection AddQuickResponseCodeApiClient(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IQuickResponseCodeGenerator, ApiClient>();
+
+            return serviceCollection;
+        }
+
+        public static IServiceCollection AddQuickResponseCodeApiClient(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection.AddQuickResponseCodeApiClient();
+            serviceCollection.Configure<ApiClient>(configuration);
+
+            return serviceCollection;
+        }
+
+        public static IServiceCollection AddQuickResponseCodeApiClient(this IServiceCollection serviceCollection, Action<ApiClient> configureOptions)
+        {
+            serviceCollection.AddQuickResponseCodeApiClient();
+            serviceCollection.Configure(configureOptions);
+
+            return serviceCollection;
+        }
+
+        public static IServiceCollection AddQuickResponseCodeApiClient(this IServiceCollection serviceCollection, string name, IConfiguration configuration)
+        {
+            serviceCollection.AddQuickResponseCodeApiClient();
+            serviceCollection.Configure<ApiClient>(name, configuration);
+
+            return serviceCollection;
+        }
+
+        public static IServiceCollection AddQuickResponseCodeApiClient(this IServiceCollection serviceCollection, string name, Action<ApiClient> configureOptions)
+        {
+            serviceCollection.AddQuickResponseCodeApiClient();
+            serviceCollection.Configure(name, configureOptions);
+
+            return serviceCollection;
+        }
+    }
+}
